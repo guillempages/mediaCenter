fb52f2432efd1967037c693c4c8cff8954421c15
Guillem Pagès Gassull <git@guillempages.cat>
2012-10-05 21:29:06 +0200

    Add dummy default plug-in

---
+++ src/mediaCenter_dummy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <unistd.h>
+
+#include "defines.h"
+#include "utils.h"
+
+int main(int argc, char * argv[]) {
+
+    DBG(
+       std::cout << argv[0];
+       for (int i=1; i < argc ; ++i) {
+           std::cout << " " << argv[i];
+       }
+       std::cout << std::endl;
+    )
+
+    while (1) {
+        sleep(1);
+    }
+}
